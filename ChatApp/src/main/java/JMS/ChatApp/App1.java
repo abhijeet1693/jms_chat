d2ff59c420924b4d6a1d2b4d2418a08378be5fac
abhijeet1693 <50795437+abhijeet1693@users.noreply.github.com>
2020-01-27 13:40:48 +0200

    Update App1.java

--- ChatApp/src/main/java/JMS/ChatApp/App1.java
+++ ChatApp/src/main/java/JMS/ChatApp/App1.java
@@ -7,6 +7,7 @@ import javax.jms.JMSException;
 public class App1 {
 	public static void main(String[] args) throws JMSException, IOException {
 		MessageUser2 user2 = new MessageUser2();
+		
 		user2.receive();
 	}
 }
